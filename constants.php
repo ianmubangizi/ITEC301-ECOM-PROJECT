eba39a4752a5712ff76b4249c2b5a9cd3cb6f441
Ian Mubangizi <io@ianmubangizi.com>
2020-08-08 19:13:42 +0200

    app constants

---
+++ constants.php
@@ -0,0 +1,14 @@
+<?php
+
+define('CART', 'CART');
+define('WISHLIST', 'WISHLIST');
+
+define('MANAGER', 'M');
+define('CUSTOMER', 'C');
+define('ANONYMOUS', 'A');
+define('CURRENCY', array(
+    'ZAR' => 'R',
+    'USD' => '$',
+    'EUR' => '€',
+    'GPB' => '£'
+));
